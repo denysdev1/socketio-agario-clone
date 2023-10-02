cd99f72285ca48f8683359b8a988191d2949c601
Denys Kleimenov <denys.kleimenov.dev@gmail.com>
2023-10-02 14:01:07 +0300

    add html markup and basic js

---
+++ public/uiStuff.js
@@ -0,0 +1,34 @@
+let wHeight = window.innerHeight;
+let wWidth = window.innerWidth;
+const canvas = document.querySelector('#the-canvas');
+const context = canvas.getContext('2d');
+const player = {};
+const loginModal = new bootstrap.Modal(document.querySelector('#loginModal'));
+const spawnModal = new bootstrap.Modal(document.querySelector('#spawnModal'));
+
+canvas.height = wHeight;
+canvas.width = wWidth;
+
+window.addEventListener('load', () => {
+  loginModal.show();
+});
+
+document.querySelector('.name-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  player.name = document.querySelector('#name-input').value;
+  document.querySelector('.player-name').innerHTML = player.name;
+  loginModal.hide();
+  spawnModal.show();
+});
+
+document.querySelector('.start-game').addEventListener('click', () => {
+  spawnModal.hide();
+
+  const elArray = document.querySelectorAll('.hiddenOnStart');
+
+  elArray.forEach((el) => {
+    el.removeAttribute('hidden');
+  });
+
+  init();
+});
